c54e7618d4cb0e5df0918a73496ead4558f7376d
Saraswathy <saraswathyysridhar@gmail.com>
2026-06-24 17:05:59 +0530

    Fix remaining merge conflict in Dashboard.js

--- student-react/src/pages/Dashboard.js
+++ student-react/src/pages/Dashboard.js
@@ -326,7 +326,6 @@ function Dashboard() {
         >
           View Approved Tips
         </button>
-<<<<<<< HEAD
 
         <button
           onClick={() => navigate("/submit")}
@@ -334,8 +333,6 @@ function Dashboard() {
         >
           Submit Tip
         </button>
-=======
->>>>>>> 7a4c5d339b264ecf2fe4cfc93e7fd2e5df5441a2
       </div>
 
       {subjects.length === 0 ? (
